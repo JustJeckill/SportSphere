df723901931d555ed82a87aa1ad58de292bc1ad5
JustJeckill <yevhenduplenko@gmail.com>
2023-11-19 17:39:53 +0000

    [main] add eslint

--- client/config/webpack/configPlugins.ts
+++ client/config/webpack/configPlugins.ts
@@ -1,11 +1,13 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {Configuration} from "webpack";
+import ESLintPlugin from "eslint-webpack-plugin";
 import {IConfigOptions} from "./types";
 
 export const configPlugins = (options: IConfigOptions): Configuration['plugins'] => {
     const { mode, paths } = options;
 
+    const isDev = mode === 'development';
     const isProd = mode === 'production';
 
     const config: Configuration['plugins'] = [];
@@ -22,6 +24,12 @@ export const configPlugins = (options: IConfigOptions): Configuration['plugins']
         });
 
         config.push(cssPlugin);
+    }
+
+    if(isDev) {
+        const eslintPlugin = new ESLintPlugin();
+
+        config.push(eslintPlugin);
     }
 
     return config;
