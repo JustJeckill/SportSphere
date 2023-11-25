f63c7b99627a972d338f86f28318ef995e8e3908
JustJeckill <yevhenduplenko@gmail.com>
2023-11-25 23:37:25 +0000

    [main] add types for store, add fetch request

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,8 +1,26 @@
 import classes from './App.module.scss';
 import { RegistrationModal } from "./modals/RegistrationModal/RegistrationModal";
 import { Outlet } from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export const App = () => {
+    const [food, setFood] = useState(null);
+
+    useEffect(() => {
+        getFood();
+    }, []);
+
+    const getFood = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+        if (response.ok) {
+            const json = await response.json();
+            console.log(json);
+        } else {
+            alert("HTTP-Error: " + response.status);
+        }
+    }
+
     return(
         <div className={classes.text}>
             <Outlet />
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -4,4 +4,7 @@ import { counterReducer } from "./reducers/counterReducer";
 
 const store = configureStore({ reducer: counterReducer })
 
-console.log(store.getState());
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
